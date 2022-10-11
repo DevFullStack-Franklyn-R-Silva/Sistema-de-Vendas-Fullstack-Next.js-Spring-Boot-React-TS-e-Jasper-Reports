24159531c65e83656225289e5b20dc3804ebf7f3
Franklyn R <franklyn1998@live.com>
2022-10-11 10:28:44 -0300

    Componente tabela de produtos

--- frontend/src/components/produtos/listagem/index.tsx
+++ frontend/src/components/produtos/listagem/index.tsx
@@ -1,13 +1,23 @@
 import { Layout } from "components";
 import Link from "next/link";
+import { TabelaProdutos } from "./tabela";
+import { Produto } from "app/models/produtos";
 
 export const ListagemProdutos: React.FC = () => {
+
+    const produtos: Produto[] = [{
+        id: "1", sku: "ABV123", nome: "Ventilador", preco: 80.00
+        
+    },
+    {id: "1", sku: "ABV123", nome: "Ventilador", preco: 80.00}]
+
     return (
         <Layout titulo="Produtos">
             <Link href="/cadastros/produtos">
                 <button className="button is-warning">Novo</button>
             </Link>
             <br />
+            <TabelaProdutos produtos={produtos}/>
         </Layout>
     );
 };
