9980b1421ca6c4bfe9698d03254d8c4e8e7902ec
Franklyn R <franklyn1998@live.com>
2022-10-14 18:51:05 -0300

    Implementando os campos do formulário de clientes

--- frontend/src/components/clientes/cadastro/form.tsx
+++ frontend/src/components/clientes/cadastro/form.tsx
@@ -1,5 +1,8 @@
 import { Cliente } from "app/models/clientes";
 import { useFormik } from "formik";
+import { Input } from "components";
+import { formatWithOptions } from "util";
+import Link from "next/link";
 
 interface ClienteFormProps {
     cliente: Cliente;
@@ -9,7 +12,7 @@ interface ClienteFormProps {
 const formScheme: Cliente = {
     cadastro: "",
     cpf: "",
-    dataNascimento: "",
+    dataNascimento: "01/01/2021",
     email: "",
     endereco: "",
     id: "",
@@ -20,27 +23,115 @@ const formScheme: Cliente = {
 export const ClienteForm: React.FC<ClienteFormProps> = ({
     cliente,
     onSubmit,
-}: ClienteFormProps) => {
+}) => {
     const formik = useFormik<Cliente>({
-        initialValues: { ...cliente, ...formScheme },
+        initialValues: { ...formScheme, ...cliente },
         onSubmit,
     });
 
     return (
         <form onSubmit={formik.handleSubmit}>
-            <input
-                value={formik.values.nome}
-                onChange={formik.handleChange}
-                id="nome"
-                name="nome"
-            />
-            <input
-                value={formik.values.cpf}
-                onChange={formik.handleChange}
-                id="cpf"
-                name="cpf"
-            />
-            <button type="submit">Enviar</button>
+            {formik.values.id && (
+                <div className="columns">
+                    <Input
+                        id="id"
+                        name="id"
+                        label="Código: "
+                        autoComplete="off"
+                        disabled={true}
+                        columnClasses="is-half"
+                        value={formik.values.id}
+                    />
+
+                    <Input
+                        id="cadastro"
+                        name="cadastro"
+                        label="Data Cadastro: "
+                        autoComplete="off"
+                        disabled={true}
+                        columnClasses="is-half"
+                        value={formik.values.cadastro}
+                    />
+                </div>
+            )}
+            <div className="columns">
+                <Input
+                    id="nome"
+                    name="nome"
+                    label="Nome: *"
+                    autoComplete="off"
+                    columnClasses="is-full"
+                    onChange={formik.handleChange}
+                    value={formik.values.nome}
+                />
+            </div>
+            <div className="columns">
+                <Input
+                    id="cpf"
+                    name="cpf"
+                    label="CPF: *"
+                    autoComplete="off"
+                    columnClasses="is-half"
+                    onChange={formik.handleChange}
+                    value={formik.values.cpf}
+                />
+
+                <Input
+                    id="dataNascimento"
+                    name="dataNascimento"
+                    label="Data Nascimento: *"
+                    autoComplete="off"
+                    columnClasses="is-half"
+                    onChange={formik.handleChange}
+                    value={formik.values.dataNascimento}
+                />
+            </div>
+            <div className="columns">
+                <Input
+                    id="endereco"
+                    name="endereco"
+                    label="Endereço: *"
+                    autoComplete="off"
+                    columnClasses="is-full"
+                    onChange={formik.handleChange}
+                    value={formik.values.endereco}
+                />
+            </div>
+            <div className="columns">
+                <Input
+                    id="email"
+                    name="email"
+                    label="E-mail: *"
+                    autoComplete="off"
+                    columnClasses="is-half"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+
+                <Input
+                    id="telefone"
+                    name="telefone"
+                    label="Telefone: *"
+                    autoComplete="off"
+                    columnClasses="is-half"
+                    onChange={formik.handleChange}
+                    value={formik.values.telefone}
+                />
+            </div>
+            <div className="field is-grouped">
+                <div className="control">
+                    <button className="button is-link" type="submit">
+                        {formik.values.id ? "Atualizar" : "Salvar"}
+                    </button>
+                </div>
+                {/* <div className="control">
+                    <Link href="/consultas/produtos">
+                        <button className="button is-link is-light">
+                            Voltar
+                        </button>
+                    </Link>
+                </div> */}
+            </div>
         </form>
     );
 };
--- frontend/src/components/clientes/cadastro/index.tsx
+++ frontend/src/components/clientes/cadastro/index.tsx
@@ -4,7 +4,10 @@ import { ClienteForm } from "./form";
 import { Cliente } from "app/models/clientes";
 
 export const CadastroCliente: React.FC = () => {
-    const [cliente, setCliente] = useState<Cliente>({});
+    const [cliente, setCliente] = useState<Cliente>({
+        nome: "Fulano",
+        cpf: "000.000.000-01"
+    });
 
     const handleSubmit = (cliente: Cliente) => {
         console.log(cliente);
