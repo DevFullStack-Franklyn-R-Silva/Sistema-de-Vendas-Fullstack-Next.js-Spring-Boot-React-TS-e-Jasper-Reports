2728db3c3d17baca3b649636d3680f0eae4af4ef
Franklyn R <franklyn1998@live.com>
2022-10-18 10:59:17 -0300

    Formulário de consulta de clientes

--- frontend/src/components/clientes/listagem/index.tsx
+++ frontend/src/components/clientes/listagem/index.tsx
@@ -1,10 +1,55 @@
-import {Layout } from "components"
+import { Input, Layout } from "components";
+import { useFormik } from "formik";
 
+interface ConsultaClientesForm {
+    nome?: string;
+    cpf?: string;
+}
 
-export const ListagemClientes: React.FC = () => { 
+export const ListagemClientes: React.FC = () => {
+    const handleSubmit = (filtro: ConsultaClientesForm) => {
+        console.log(filtro);
+    };
+
+    const {
+        handleSubmit: formikSubmit,
+        values: filtro,
+        handleChange,
+    } = useFormik<ConsultaClientesForm>({
+        onSubmit: handleSubmit,
+        initialValues: { nome: "", cpf: "" },
+    });
+
     return (
         <Layout titulo="Clientes">
-            
+            <form onSubmit={formikSubmit}>
+                <div className="columns">
+                    <Input
+                        label="Nome"
+                        id="nome"
+                        name="nome"
+                        value={filtro.nome}
+                        columnClasses="is-half"
+                        onChange={handleChange}
+                        autoComplete="off"
+                    />
+                    <Input
+                        label="CPF"
+                        id="cpf"
+                        name="cpf"
+                        value={filtro.cpf}
+                        columnClasses="is-half"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button className="button is-link" type="submit">
+                            Consultar
+                        </button>
+                    </div>
+                </div>
+            </form>
         </Layout>
-    )
-}
+    );
+};
